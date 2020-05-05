213db429230106cbe5fa4437bede99fc02d35cc5
Riley Worstell <rcwors02@gmail.com>
2020-05-04 22:01:55 -0400

    Initial Commit

---
+++ MineSweep/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MineSweep
+//
+//  Created by Riley Worstell on 5/4/20.
+//  Copyright © 2020 Riley Worstell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
